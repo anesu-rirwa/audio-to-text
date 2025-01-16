42c5e25770534a4d38ffe3cbea13fd55f2e0dd2c
Anesu Rirwa <anerirwa@gmail.com>
2025-01-16 10:49:43 +0200

    Added File Upload html template

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ from flask import Flask, render_template, request
 import torch # PyTorch 
 from transformers import pipeline # Hugging Face Transformers for NLP
 import soundfile as sf # PySoundFile for reading audio file
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Home Route
+@app.route('/')
+def home():
+    return render_template('index.html')
